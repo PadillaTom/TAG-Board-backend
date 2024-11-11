193efffaf2baca877348c7aabcf405483883a5ea
Tomas Padilla <padillatomasagustin@gmail.com>
2024-11-11 12:26:46 +0100

    [TBB-5] - Codestyle Fix

--- src/main/java/com/padillatom/TAG_Board/config/SecurityConstants.java
+++ src/main/java/com/padillatom/TAG_Board/config/SecurityConstants.java
@@ -9,5 +9,4 @@ public class SecurityConstants {
     public static final String ISSUER = "Tag-Solution";
     public static final String SECRET = "jxgEQeXHuPq8VdbyYFNkANdudQ53YUn4";
     public static final long EXPIRATION_TIME = 864_000_000; // 10 days
-
 }
